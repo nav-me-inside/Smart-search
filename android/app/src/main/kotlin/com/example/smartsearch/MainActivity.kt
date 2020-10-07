229eacda0e188075a766b785583bde852e119428
Jagmohan Rai <aahhansingh@gmail.com>
2020-10-07 23:23:13 +0530

    search screen added

---
+++ android/app/src/main/kotlin/com/example/smartsearch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smartsearch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,2 @@
+const url =
+    'guide OR tutorial OR course OR example OR build OR create OR started -site:github.com -site:github.io -site:npmjs.com -site:openbase.io -site:stackoverflow.com -site:codota.com -site:pika.dev -site:bountysource.com -site:unpkg.com -site:snyk.io -site:gitter.im';
